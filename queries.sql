880f81761e4ddee20270f8a5d2f8e7a9b711d1a4
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-08-22 16:24:45 +0200

    Analyze database performance

--- queries.sql
+++ queries.sql
@@ -1,5 +1,3 @@
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits WHERE animal_id = 4;
-
 EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
-
 EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
--- schema.sql
+++ schema.sql
@@ -1,7 +1,3 @@
-
-
 CREATE INDEX idx_animal_id ON visits(animal_id);
-
 CREATE INDEX idx_vet_id ON visits(vet_id);
-
 CREATE INDEX idx_email ON owners(email);
